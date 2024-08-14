c44215bc1d65d6a8f3544116eabf68b0107bbab2
ChangHunYu <frenzi1@naver.com>
2024-08-14 16:26:10 +0900

    로그인 구현 (Jwt 토큰)

--- src/test/java/ezen/risk_buster/hazard_hackers/UserTest.java
+++ src/test/java/ezen/risk_buster/hazard_hackers/UserTest.java
@@ -1,15 +1,16 @@
 package ezen.risk_buster.hazard_hackers;
 
-import ezen.risk_buster.hazard_hackers.user.LoginRequest;
-import ezen.risk_buster.hazard_hackers.user.User;
-import ezen.risk_buster.hazard_hackers.user.UserRepository;
-import ezen.risk_buster.hazard_hackers.user.UserResponseDTO;
+import ezen.risk_buster.hazard_hackers.common.auth.JwtProvider;
+import ezen.risk_buster.hazard_hackers.common.auth.SecurityUtils;
+import ezen.risk_buster.hazard_hackers.user.*;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.apache.http.HttpHeaders;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,10 +32,16 @@ public class UserTest {
     @LocalServerPort
     int port;
 
+    static String rawPassword = "password1";
+    static String hasedPassword1;
+    static User 유저1;
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+        hasedPassword1 = SecurityUtils.sha256Encrypt("password1");
+        유저1 = new User("young", "abc@gmail.com", hasedPassword1);
     }
+
 
     @Autowired
     UserRepository userRepository;
@@ -46,7 +53,6 @@ public class UserTest {
     @DisplayName("User 생성 테스트")
     void createUser() {
         //given - User 객체를 생성한다.
-        User 유저1 = new User("young", "abc@gmail.com", "password1");
 
         //when - 생성한 User 객체를 저장한다.
         User 저장된_유저 = userRepository.save(유저1);
@@ -55,15 +61,14 @@ public class UserTest {
         //then - 검증
         assertThat(저장된_유저.getUsername()).isEqualTo(유저1.getUsername());
         assertThat(저장된_유저.getEmail()).isEqualTo(유저1.getEmail());
-        assertThat(저장된_유저.getPassword()).isEqualTo(유저1.getPassword());
+        assertThat(저장된_유저.getPassword()).isEqualTo(hasedPassword1);
     }
 
     @Test
     @DisplayName("로그인 테스트")
     void login() {
         // given
-        User 유저1 = userRepository.save(new User( "young", "abc@gmail.com", "password1"));
-        LoginRequest loginRequest = new LoginRequest(유저1.getEmail(), 유저1.getPassword());
+        LoginRequest loginRequest = new LoginRequest(유저1.getEmail(), rawPassword);
 
         //when & then
         RestAssured
@@ -80,7 +85,6 @@ public class UserTest {
     @DisplayName("User 조회 테스트")
     void findUserByUserId() {
         //given - User 객체를 생성 + 저장한다.
-        User 유저1 = userRepository.save(new User("young", "abc@gmail.com", "password1"));
 
         //when - 저장된 User의 userId로 조회한다.
         User 찾은_User = userRepository.findByIdAndIsDeletedFalse(유저1.getId());
@@ -95,7 +99,6 @@ public class UserTest {
     @DisplayName("프로필 수정 테스트")
     void update() {
         // given
-        User 유저1 = userRepository.save(new User("young", "abc@gmail.com", "password1"));
         UserResponseDTO request = new UserResponseDTO(
                 유저1.getId(),
                 유저1.getUsername(),
@@ -117,7 +120,6 @@ public class UserTest {
     @Test
     @DisplayName("회원탈퇴 테스트")
     void delete() {
-        User 유저1 = userRepository.save(new User("young", "abc@gmail.com", "password1"));
         RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
@@ -133,5 +135,43 @@ public class UserTest {
                 .get("/users/" + 유저1.getId())
                 .then().log().all()
                 .statusCode(500).extract();
+    }
+
+    @Test
+    @DisplayName("프로필 조회 테스트")
+    void getCurrentUser() {
+        //given
+        // User 저장
+        User 저장된_유저 = userRepository.save(유저1);
+        em.clear();
+
+        // 로그인 후 토큰 발급
+        LoginRequest login = new LoginRequest(유저1.getEmail(), rawPassword);
+        ExtractableResponse<Response> extract = RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(login)
+                .when()
+                .post("/users/login")
+                .then().log().all()
+                .statusCode(200).extract();
+        LoginResponse token = extract.as(LoginResponse.class);
+
+        //when
+        // 프로필 조회
+        ExtractableResponse<Response> extract1 = RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token.accessToken())
+                .body(login)
+                .when()
+                .get("/users/me")
+                .then().log().all()
+                .statusCode(200).extract();
+        UserResponseDTO responseDTO = extract1.as(UserResponseDTO.class);
+        Assertions.assertThat(responseDTO).isNotNull();
+        Assertions.assertThat(responseDTO.name()).isEqualTo(유저1.getUsername());
+        Assertions.assertThat(responseDTO.email()).isEqualTo(유저1.getEmail());
+
     }
 }
