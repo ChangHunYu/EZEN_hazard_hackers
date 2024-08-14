f762b8ddafa0fa5f2d0ee8732a19bc44cc5fc920
jihye <88qnddj@gmail.com>
2024-08-14 16:08:45 +0900

    ItemTest에 ActiveProfiles 추가

--- src/test/java/ezen/risk_buster/hazard_hackers/ItemTest.java
+++ src/test/java/ezen/risk_buster/hazard_hackers/ItemTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
 
@@ -27,6 +28,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Sql("/truncate.sql")
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ItemTest {
 
