c97aad50a2c42c226f0afe51149ca88f525ae22b
cy0112c <tmznt11@naver.com>
2024-08-16 18:04:56 +0900

    Itinerary Entity에 Checklist 1:1관계 설정

--- src/main/java/ezen/risk_buster/hazard_hackers/itinerary/Itinerary.java
+++ src/main/java/ezen/risk_buster/hazard_hackers/itinerary/Itinerary.java
@@ -1,5 +1,6 @@
 package ezen.risk_buster.hazard_hackers.itinerary;
 
+import ezen.risk_buster.hazard_hackers.checklist.Checklist;
 import ezen.risk_buster.hazard_hackers.common.BaseEntity;
 import ezen.risk_buster.hazard_hackers.country.Country;
 import ezen.risk_buster.hazard_hackers.user.User;
@@ -38,11 +39,18 @@ public class Itinerary extends BaseEntity {
 
     private String description;
 
+    @OneToOne(mappedBy = "itinerary", cascade = CascadeType.ALL)
+    private Checklist checklist;
+
     //setter로 저장해서 리턴하면 수정할수있는 부분으로 혼동할수 있기 때문에 update함수를 만들어서 사용
     public void update(ItineraryRequest request) {
         this.title = request.title();
         this.startDate = request.startDate();
         this.endDate = request.endDate();
         this.description = request.description();
+    }
+
+    public void addCheklist(Checklist checklist) {
+        this.checklist = checklist;
     }
 }
